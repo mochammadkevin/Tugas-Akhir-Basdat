897afd641aaf5ceced7f312e9ff06068e2de42b2
Kevin <88078382+mochammadkevin@users.noreply.github.com>
2022-11-23 02:41:28 +0700

    Update registrasi.php

--- registrasi.php
+++ registrasi.php
@@ -32,7 +32,7 @@ if(isset($_POST['register'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register Page</title>
+    <title>IPB Garage</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
@@ -47,8 +47,26 @@ if(isset($_POST['register'])){
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="login.php">IPB Garage</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="registrasi.php">Registrasi</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <div class="mx-auto">
-        <!-- untuk memasukkan data -->
         <div class="card">
             <div class="card-header">
                 Registrasi Akun
