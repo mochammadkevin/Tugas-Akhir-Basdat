d2dc1328ef3b58a98e2fbd6c8d46006f76743548
Kevin <88078382+mochammadkevin@users.noreply.github.com>
2022-11-23 02:39:41 +0700

    Update berhasil.php

--- berhasil.php
+++ berhasil.php
@@ -1,13 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>IPB Garage</title>
+    <style>
+    .mx-auto {
+        width: 800px;
+    }
+
+    .card {
+        margin-top: 10px;
+    }
+    </style>
 </head>
+
 <body>
-    <h1>Login berhasil!</h1>
-    <a href="logout.php">Logout</a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="login.php">IPB Garage</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="">Main Page</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="mx-auto">
+        <div class="card">
+            <div class="alert alert-success" role="alert">
+                <h4 class="alert-heading">Login Berhasil!</h4>
+                <p>Namun, website ini belum sepenuhnya selesai..</p>
+                <hr>
+                <p class="mb-0">Mohon ditunggu ya!</p>
+            </div>
+            <a class="btn btn-danger" href="logout.php" role="button">Log out</a>
+        </div>
+    </div>
 </body>
+
 </html>
