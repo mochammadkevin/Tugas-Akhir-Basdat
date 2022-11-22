109388bc3ed002a8c2a62d1b3fd931bdc3931bc4
Kevin <88078382+mochammadkevin@users.noreply.github.com>
2022-11-23 02:40:00 +0700

    Update form-pembeli-motor.php

--- form-pembeli-motor.php
+++ form-pembeli-motor.php
@@ -28,12 +28,12 @@ if(isset($_POST['submit'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Pembeli Motor</title>
+    <title>IPB Garage</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
     .mx-auto {
-        width: 800px;
+        width: 850px;
     }
 
     .card {
@@ -43,8 +43,23 @@ if(isset($_POST['submit'])){
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="login.php">IPB Garage</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="form-pembeli.php">Registrasi Data</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
     <div class="mx-auto">
-        <!-- untuk memasukkan data -->
         <div class="card">
             <div class="card-header">
                 Registrasi Data Pembeli Motor
