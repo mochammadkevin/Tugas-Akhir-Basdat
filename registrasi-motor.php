5e503efe5a11e8a871bd100053bf012fd1266a37
mochammadkevin <mochammadkevin@apps.ipb.ac.id>
2022-11-25 02:33:42 +0700

    update regist dikit

--- registrasi-motor.php
+++ registrasi-motor.php
@@ -51,9 +51,13 @@ if(isset($_POST['submit'])){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
     <style>
     .mx-auto {
-        width: 850px;
+        width: 1000px;
     }
 
     .card {
