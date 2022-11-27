1f9f01182acbeeca9d5814af6d12c85337f49427
rifqifazzam <rifqifauzan709@gmail.com>
2022-11-27 14:32:57 +0700

    ngotak ngatik logout dikit

--- logout.php
+++ logout.php
@@ -2,7 +2,9 @@
 require 'function.php';
 $_SESSION = [];
 session_unset();
+// session_start();
 session_destroy();
 header("Location: index.php");
+// echo '<script>window.location="index.php"</script>';
 exit;
 ?>
