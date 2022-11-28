76c40cb94cbc191a29d7e4a703b92d4bf3d4b78b
mochammadkevin <mochammadkevin@apps.ipb.ac.id>
2022-11-29 01:20:48 +0700

    update

--- registrasi.php
+++ registrasi.php
@@ -40,7 +40,7 @@ if(isset($_POST['register'])){
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="style/style.css?v=<?php echo time(); ?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
@@ -88,9 +88,9 @@ if(isset($_POST['register'])){
                             <label for="alamat" class="form-label">Alamat</label>
                             <input type="alamat" class="form-control" id="alamat" name="alamat"
                                 placeholder="Masukkan alamat" value="">
-                            <label for="email" class="form-label">Email</label>
+                            <label for="email" class="form-label">E-mail</label>
                             <input type="email" class="form-control" id="email" name="email"
-                                placeholder="Masukkan email" value="" required>
+                                placeholder="Masukkan e-mail" value="" required>
                             <label for="nomor_tlp" class="form-label">Nomor Telepon</label>
                             <input type="nomor_tlp" class="form-control" id="nomor_tlp" name="nomor_tlp"
                                 placeholder="Masukkan nomor telepon" value="" required>
