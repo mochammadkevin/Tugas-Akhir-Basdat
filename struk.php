17f27883a4bb0d5736ca98fe84f7b228b45bb847
mochammadkevin <mochammadkevin@apps.ipb.ac.id>
2022-11-28 23:47:18 +0700

    update

--- struk.php
+++ struk.php
@@ -1,10 +1,9 @@
 <?php 
 session_start();
-if( !isset($_SESSION["login"]) ) {
-    header("Location: index.php");
-    exit;
-  }
-include 'function.php';
+if ($_SESSION['login'] != true) {
+    echo '<script>window.location="index.php"</script>';
+}
+require 'function.php';
 
 $sql = "SELECT * FROM user WHERE id_user = $_SESSION[id_user]";
 $query = mysqli_query($koneksi, $sql);
@@ -12,37 +11,6 @@ $data = mysqli_fetch_array($query);
 $id_user = $data['id_user'];
 $username = $data['username'];
 $nama_lengkap = $data['nama_lengkap'];
-
-if (isset($_GET['idm'])) {
-    $idm = $_GET['idm'];
-    $sql = "SELECT * FROM motor WHERE id_motor = $idm and id_user = $_SESSION[id_user]";
-
-    $motor = mysqli_query($koneksi, "SELECT * FROM motor WHERE id_motor = '$_GET[idm]'");
-    $m = mysqli_fetch_object($motor);
-
-    $idp = $m->id_user; 
-    $nama_motor = $m->nama_motor;
-    $merk_motor = $m->merk_motor;
-    $jenis_motor = $m->jenis_motor;
-    $CC_motor = $m->CC_motor;
-    $gambar_motor = $m->gambar_motor;
-    $tahun_keluaran = $m->tahun_keluaran;
-    $jarak_tempuh = $m->jarak_tempuh;
-    $harga_motor = $m->harga_motor;
-    $deskripsi_motor = $m->deskripsi_motor;
-
-    $sql2 = "SELECT * FROM user WHERE id_user = $idp";
-    $query2 = mysqli_query($koneksi, $sql2);
-
-    $user = mysqli_query($koneksi, "SELECT * FROM user WHERE id_user = $idp");
-    $u = mysqli_fetch_object($user);
-
-    $nama_p = $u->nama_lengkap;
-    $email_p = $u->email;
-    $tlp_p= $u->nomor_tlp;
-    $alamat_p = $u->alamat_rumah;
-}
-
 ?>
 
 <!DOCTYPE html>
@@ -58,11 +26,11 @@ if (isset($_GET['idm'])) {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
         rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
     <title>IPB Garage</title>
     <style>
     .mx-auto {
-        width: 1000px;
+        width: 1400px;
     }
 
     .card {
@@ -88,11 +56,15 @@ if (isset($_GET['idm'])) {
                         <a class="nav-link" aria-current="page" href="data-motor.php">Jual Motor</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="transaksi.php">Transaksi</a>
+                        <a class="nav-link" aria-current="page" href="keranjang.php">Keranjang</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="">Struk</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" aria-current="page" href="logout.php">Log out</a>
                     </li>
+                    <!-- ini float in right dungs -->
                     <li class="nav-item">
                         <a class="nav-link" aria-current="page" ><?php echo $username ?></a>
                     </li>
@@ -103,109 +75,56 @@ if (isset($_GET['idm'])) {
     <div class="mx-auto">
         <div class="card">
             <div class="card-header">
-                Detail Motor
+                <!-- Motor yang telah dipesan -->
             </div>
             <div class="card-body">
-                <div class="mb-3 row">
-                    <label for="merk_motor" class="col-sm-2 col-form-label">Merk</label>
-                    <div class="col-sm-10">
-                        <input type="text" readonly class="form-control-plaintext" id="merk_motor"
-                            value="<?php echo $merk_motor; ?>">
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <label for="nama_motor" class="col-sm-2 col-form-label">Nama</label>
-                    <div class="col-sm-10">
-                        <input type="text" readonly class="form-control-plaintext" id="nama_motor"
-                            value="<?php echo $nama_motor; ?>">
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <label for="merk_motor" class="col-sm-2 col-form-label">Jenis</label>
-                    <div class="col-sm-10">
-                        <input type="text" readonly class="form-control-plaintext" id="merk_motor"
-                            value="<?php echo $jenis_motor; ?>">
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <label for="CC_motor" class="col-sm-2 col-form-label">CC</label>
-                    <div class="col-sm-10">
-                        <input type="text" readonly class="form-control-plaintext" id="CC_motor"
-                            value="<?php echo $CC_motor; ?>">
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <label for="merk_motor" class="col-sm-2 col-form-label">Tahun Keluaran</label>
-                    <div class="col-sm-10">
-                        <input type="text" readonly class="form-control-plaintext" id="merk_motor"
-                            value="<?php echo $tahun_keluaran; ?>">
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <label for="merk_motor" class="col-sm-2 col-form-label">Jarak tempuh</label>
-                    <div class="col-sm-10">
-                        <input type="text" readonly class="form-control-plaintext" id="merk_motor"
-                            value="<?php echo $jarak_tempuh; ?>">
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <label for="merk_motor" class="col-sm-2 col-form-label">Harga</label>
-                    <div class="col-sm-10">
-                        <input type="text" readonly class="form-control-plaintext" id="merk_motor"
-                            value="Rp. <?php echo $harga_motor; ?>">
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <label for="merk_motor" class="col-sm-2 col-form-label">Deskripsi</label>
-                    <div class="col-sm-10">
-                        <input type="text" readonly class="form-control-plaintext" id="merk_motor"
-                            value="<?php echo $deskripsi_motor; ?>">
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <label for="merk_motor" class="col-sm-2 col-form-label">Gambar</label>
-                    <div class="col-sm-10">
-                        <img src="images/<?php echo $m->gambar_motor ?>" width="300px">
-                    </div>
-                </div>
-                </form>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Merk</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">Gambar</th>
+                            <th scope="col">Harga</th>
+                            <th scope="col">Nama Ekspedisi</th>
+                            <th scope="col">Tanggal Transaksi</th>
+                        </tr>
+                    <tbody>
+                        <?php
+                            $id_user = $_SESSION['id_user'];
+                            // Masukin motor yang dia pesen
+                            $sql5 = "SELECT * FROM transaksi INNER JOIN motor USING(id_motor) INNER JOIN shipment USING(id_shipment) WHERE transaksi.id_user = $id_user";
+            
+                            $query5 = mysqli_query($koneksi, $sql5);
+                            $urut = 1;
+                            while ($r5 = mysqli_fetch_array($query5)) {
+                                $ids = $r5['id_shipment'];
+                                $id_motor = $r5['id_motor'];
+                                $nama_motor = $r5['nama_motor'];
+                                $merk_motor = $r5['merk_motor'];
+                                $gambar_motor = $r5['gambar_motor'];
+                                $harga_motor = $r5['harga_motor'];
+                                $nama_ekspedisi = $r5['nama_ekspedisi'];
+
+                                $tanggal_transaksi = $r5['tanggal_transaksi'];
+                                
+
+                        ?>
+                        <tr>
+                            <th scope="row"><?php echo $urut++ ?></th>
+                            <td><?php echo $merk_motor ?></td>
+                            <td><?php echo $nama_motor ?></td>
+                            <td><img src="images/<?php echo $gambar_motor ?>" width="300px" ></td>
+                            <td>Rp. <?php echo $harga_motor ?></td>
+                            <td><?php echo $nama_ekspedisi ?></td>
+                            <td><?php echo $tanggal_transaksi ?></td>
+                        </tr>
+                        <?php } ?>
+                    </tbody>
+                    </thead>
+                </table>
             </div>
         </div>
-    </div>
-    <div class="mx-auto">
-        <div class="card">
-            <div class="card-header">
-                Detail Penjual
-            </div>
-            <div class="card-body">
-                <div class="mb-3 row">
-                    <label for="nama_lengkap" class="col-sm-2 col-form-label">Nama Lengkap</label>
-                    <div class="col-sm-10">
-                        <input type="text" readonly class="form-control" id="nama_lengkap" value="<?php echo $nama_p?>">
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <label for="alamat_rumah" class="col-sm-2 col-form-label">Alamat Rumah</label>
-                    <div class="col-sm-10">
-                        <input type="text" readonly class="form-control" id="alamat_rumah" value="<?php echo $alamat_p ?>">
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <label for="nama_lengkap" class="col-sm-2 col-form-label">Email</label>
-                    <div class="col-sm-10">
-                        <input type="text" readonly class="form-control" id="nama_lengkap" value="<?php echo $email_p ?>">
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <label for="nomor_tlp" class="col-sm-2 col-form-label">Nomor Telepon</label>
-                    <div class="col-sm-10">
-                        <input type="text" readonly class="form-control" id="nomor_tlp" value="<?php echo $tlp_p ?>">
-                    </div>
-                </div>
-                </form>
-            </div>
-        </div>
-    </div>
 </body>
 
-</html>
+</html>`
